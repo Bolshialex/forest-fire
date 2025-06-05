aba92c1ddc5250f455bfbed954ed2ede2a23546b
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-06-05 11:46:01 -0700

    Added tests

--- src/FireTest.java
+++ src/FireTest.java
@@ -1,4 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -73,5 +79,66 @@ public class FireTest {
         int actual = Fire.timeToBurn(forest, matchR, matchC);
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPossibleMoves_oneOutcome(){
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 3;
+        int matchC = 0;
+
+
+
+        List<int[]> actual = Fire.possibleMoves(forest, matchR, matchC, new boolean[forest.length][forest[0].length]);
+        
+        Set<String> pathSet = new HashSet<>();
+
+        for (int[] pair : actual) {
+            pathSet.add(Arrays.toString(pair));
+        }
+        assertEquals(1, actual.size());
+        assertTrue(pathSet.contains("[3, 1]"));
+    }
+
+    @Test
+    public void testPossibleMoves_multiOutcome(){
+        char[][] forest = {
+            {'t','.','.','t','t','t','t','.','t'},
+            {'.','.','t','t','.','.','.','.','t'},
+            {'.','.','t','t','t','t','t','t','t'},
+            {'t','t','t','t','.','.','.','.','.'}
+        };
+
+        int matchR = 2;
+        int matchC = 3;
+
+
+
+        List<int[]> actual = Fire.possibleMoves(forest, matchR, matchC, new boolean[forest.length][forest[0].length]);
+        
+        Set<String> pathSet = new HashSet<>();
+
+        for (int[] pair : actual) {
+            pathSet.add(Arrays.toString(pair));
+        }
+        assertEquals(4, actual.size());
+        assertTrue(pathSet.contains("[2, 4]"));
+        assertTrue(pathSet.contains("[2, 2]"));
+        assertTrue(pathSet.contains("[1, 3]"));
+        assertTrue(pathSet.contains("[3, 3]"));
+    }
+
+    private Set<String> toSet(List<int[]> list) {
+        Set<String> set = new HashSet<>();
+        for (int[] arr : list) {
+            set.add(arr[0] + "," + arr[1]);
+        }
+        return set;
     }
 }
